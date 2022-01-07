3cc75fb0819ad6b7940bcd9d261444262e71ba85
shuheb <37620997+shuheb@users.noreply.github.com>
2022-01-07 21:02:30 +0000

    changing pomodoro type will also stop timer

--- src/components/PomodoroTypeComponent.js
+++ src/components/PomodoroTypeComponent.js
@@ -3,13 +3,25 @@ import { PomodoroContext } from '../PomodoroContext';
 
 const PomodoroTypeComponent = memo(props => {
   console.log('PomodoroTypeComponent');
-  const { changePomodoroType } = useContext(PomodoroContext);
+  const { changePomodoroType, stopTimer } = useContext(PomodoroContext);
   const type = props.node.data.type;
 
+  const clickHandler = (pomodoroType) => {
+    if (pomodoroType === 'long_break') {
+      changePomodoroType({ type: 'long_break', id: props.node.data.id, previous: props.node.data.type })
+    }
+    else if (pomodoroType === 'short_break') {
+      changePomodoroType({ type: 'short_break', id: props.node.data.id, previous: props.node.data.type })
+    } else {
+      changePomodoroType({ type: 'pomodoro', id: props.node.data.id, previous: props.node.data.type })
+    }
+    stopTimer({ id: props.node.data.id, timerStarted: props.node.data.timerStarted });
+  }
+
   return (<div className="p-title">
-    <button onClick={() => changePomodoroType({ type: 'pomodoro', id: props.node.data.id, previous: props.node.data.type })} className={type === "pomodoro" ? "p-title-item active" : "p-title-item"}>Pomodoro</button>
-    <button onClick={() => changePomodoroType({ type: 'short_break', id: props.node.data.id, previous: props.node.data.type })} className={type === "short_break" ? "p-title-item active" : "p-title-item"}>Short Break</button>
-    <button onClick={() => changePomodoroType({ type: 'long_break', id: props.node.data.id, previous: props.node.data.type })} className={type === "long_break" ? "p-title-item active" : "p-title-item"}>Long Break</button>
+    <button onClick={() => clickHandler('pomodoro')} className={type === "pomodoro" ? "p-title-item active" : "p-title-item"}>Pomodoro</button>
+    <button onClick={() => clickHandler('short_break')} className={type === "short_break" ? "p-title-item active" : "p-title-item"}>Short Break</button>
+    <button onClick={() => clickHandler('long_break')} className={type === "long_break" ? "p-title-item active" : "p-title-item"}>Long Break</button>
   </div>)
 })
 
