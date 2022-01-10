d0e74c99036c7546b11647725e112748979df578
shuheb <37620997+shuheb@users.noreply.github.com>
2022-01-10 19:29:11 +0000

    using ThemeContext for action button

--- src/components/ActionComponent.js
+++ src/components/ActionComponent.js
@@ -1,16 +1,17 @@
 import { memo, useContext } from 'react';
 import { PomodoroContext } from '../PomodoroContext';
+import { ThemeContext } from '../ThemeContext';
 
 const ActionComponent = memo((props) => {
     console.log('ActionComponent')
     const { startTimer, stopTimer } = useContext(PomodoroContext);
+    const themes = useContext(ThemeContext);
     const type = props.node.data.type;
+    const { background } = themes[type]
     const timerStarted = props.node.data.timerStarted;
-
-    const classNameExpression = type === "pomodoro" ? "p-button red" : type === "short_break" ? "p-button green" : "p-button blue";
     return (
-        <div className="btn-container">
-            <button className={classNameExpression}
+        <div className="btn-container" >
+            <button className="p-button" style={{color: background}}
                 onClick={() => timerStarted ? stopTimer({ id: props.node.data.id, timerStarted }) : startTimer({ id: props.node.data.id, timerStarted })}>
                 {timerStarted ? 'STOP' : 'START'}
             </button>
