61493b874f075a1f5b62f020cb8ab971321bcc98
shuheb <37620997+shuheb@users.noreply.github.com>
2022-01-11 13:18:49 +0000

    second iteration of highlight pomodoro component

--- src/components/PomodoroCellRendererComponent.js
+++ src/components/PomodoroCellRendererComponent.js
@@ -1,27 +1,19 @@
 import { useState, useEffect, useContext, memo } from 'react';
 import { PomodoroContext } from '../PomodoroContext';
 import { ThemeContext } from '../ThemeContext';
+import ActionComponent from './ActionComponent';
+import TypeComponent from './TypeComponent';
 
 const PomodoroCellRendererComponent = () => {
-  const [startTimer, setStartTimer] = useState(false);
-  const [seconds, setSeconds] = useState(60);
-  const [timerType, setTimerType] = useState("pomodoro")
-  const { rowData, currentRow } = useContext(PomodoroContext);
+  const { rowData, currentRow, changePomodoroType, startTimer, stopTimer } = useContext(PomodoroContext);
   const themes = useContext(ThemeContext);
   const [rowInfo, setRowInfo] = useState(null);
-  const { type, timeLeft } = rowInfo || '';
-
+  const { type, timeLeft, id, task, timerStarted } = rowInfo ? rowInfo : '';
   useEffect(() => {
     if (currentRow !== -1) {
       setRowInfo(rowData.filter(row => row.id === currentRow)[0])
     }
   }, [currentRow, rowData])
-
-
-
-  const onClickHandler = () => {
-    setStartTimer(prev => !prev);
-  }
 
   const secondsToShow = (timeLeft % 60) < 10 ? `0${timeLeft % 60}` : timeLeft % 60;
   const minutesToShow = Math.floor(timeLeft / 60) < 10 ? `0${Math.floor(timeLeft / 60)}` : Math.floor(timeLeft / 60);
@@ -29,22 +21,18 @@ const PomodoroCellRendererComponent = () => {
 
 
   // className={timerType === "pomodoro" ? "p-background red" : timerType === "short_break" ? "p-background green" : "p-background blue"}
-  return (currentRow !== -1 && <div className="p-background" style={{ backgroundColor: themes[type] ? themes[type].background : '' }} >
-    <div className="p-container">
-      <div className="p-title">
-        <button onClick={() => setTimerType('pomodoro')} className={timerType === "pomodoro" ? "p-title-item active" : "p-title-item"}>Pomodoro</button>
-        <button onClick={() => setTimerType('short_break')} className={timerType === "short_break" ? "p-title-item active" : "p-title-item"}>Short Break</button>
-        <button onClick={() => setTimerType('long_break')} className={timerType === "long_break" ? "p-title-item active" : "p-title-item"}>Long Break</button>
+  return (currentRow !== -1 &&
+    <div className="p-background" style={{ backgroundColor: themes[type] ? themes[type].background : '' }} >
+      <div className="p-container">
+        <TypeComponent type={type} id={id} changePomodoroType={changePomodoroType} />
+        <div>Currently Working on: {task}</div>
+        <div>current ID: {JSON.stringify(currentRow)}</div>
+        <div className="p-timer">{timerString}</div>
+        <div>
+          <ActionComponent background={themes[type] ? themes[type].background : ''} id={id} startTimer={startTimer} stopTimer={stopTimer} timerStarted={timerStarted} />
+        </div>
       </div>
-      <div>current ID: {JSON.stringify(currentRow)}</div>
-      {/* <div>rowData: {JSON.stringify(rowData)}</div> */}
-      <div className="p-timer">{timerString}</div>
-
-      <div>
-        <button className={timerType === "pomodoro" ? "p-button red" : timerType === "short_break" ? "p-button green" : "p-button blue"} onClick={onClickHandler}>{startTimer ? 'STOP' : 'START'}</button>
-      </div>
-    </div>
-  </div>)
+    </div>)
 }
 
 export default PomodoroCellRendererComponent;
