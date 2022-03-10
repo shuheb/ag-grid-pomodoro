9f09a2f8ce7cdd3b14ea5e9014ad767aa91c6d74
shuheb <37620997+shuheb@users.noreply.github.com>
2022-03-10 11:44:15 +0000

    custom hook

--- src/components/cell-renderers/TimerCellRenderer.js
+++ src/components/cell-renderers/TimerCellRenderer.js
@@ -1,7 +1,45 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { useState, useEffect, memo, useContext } from 'react';
+import { useEffect, memo, useContext } from 'react';
 import { PomodoroContext } from '../../context/PomodoroContext';
 import { formatSecondsIntoMinutesAndSeconds } from '../../utils/date';
+import useTimer from '../../utils/useTimer';
+
+
+
+const TimerCellRenderer = memo(props => {
+  const { stopTimer, persistSeconds, markAsComplete } = useContext(PomodoroContext);
+  const { id, timerStarted, timeLeft } = props.node.data;
+  const [seconds] = useTimer(timerStarted, timeLeft);
+  
+  useEffect(() => {
+    if (!timerStarted) {
+      persistSeconds({ id, timeLeft: seconds })
+    };
+  }, [timerStarted, persistSeconds, seconds, id]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      stopTimer({ id });
+      markAsComplete({ id });
+    }
+  }, [seconds, stopTimer, markAsComplete, id])
+
+  // format the seconds into minutes and seconds
+  let timeString = formatSecondsIntoMinutesAndSeconds(seconds);
+
+  return (<>
+    <div
+      style={{
+        fontSize: '25px',
+        fontWeight: 'bold'
+      }}>
+      {timeString}
+    </div>
+    <ProgressComponent timeLeft={seconds} />
+  </>)
+
+});
+
 
 // https://mui.com/components/progress/#circular-with-label
 function CircularProgressWithLabel(props) {
@@ -36,55 +74,5 @@ const ProgressComponent = memo(props => {
   return (<CircularProgressWithLabel sx={{ color: 'white' }} value={minutes} />);
 });
 
-const TimerCellRenderer = memo(props => {
-  const { stopTimer, persistSeconds, markAsComplete } = useContext(PomodoroContext);
-  const { id, timerStarted, timeLeft } = props.node.data;
-  const [seconds, setSeconds] = useState(timeLeft);
-
-  // when timer is stopped, get the time left for the active task and persist it to the store
-  useEffect(() => {
-    if (!timerStarted) {
-      persistSeconds({ id, timeLeft: seconds })
-    };
-  }, [timerStarted, persistSeconds, seconds, id]);
-
-  // start ticking the timer by decrementing every second
-  // if seconds reaches 0, then stop the timer and mark the task as completed
-  useEffect(() => {
-    let timer;
-
-    if (timerStarted) {
-      timer = setInterval(() => {
-        setSeconds(prev => prev - 1)
-      }, 1000);
-    }
-
-    return () => {
-      if (timer) { clearInterval(timer); };
-    }
-  }, [seconds, timerStarted]);
-
-  useEffect(() => {
-    if (seconds === 0) {
-      stopTimer({ id });
-      markAsComplete({ id });
-    }
-  }, [seconds, stopTimer, markAsComplete, id])
-
-  // format the seconds into minutes and seconds
-  let timeString = formatSecondsIntoMinutesAndSeconds(seconds);
-
-  return (<>
-    <div
-      style={{
-        fontSize: '25px',
-        fontWeight: 'bold'
-      }}>
-      {timeString}
-    </div>
-    <ProgressComponent timeLeft={seconds} />
-  </>)
-
-});
 
 export default TimerCellRenderer;
